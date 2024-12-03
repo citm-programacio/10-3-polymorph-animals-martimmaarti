b467dd9ae420f1bd9011177c78f480afe3e33d61
martina <martina.bo@estudiantat.upc.edu>
2024-12-03 22:47:39 +0100

    Update 10.3 polymorph animals.cpp

--- 10.3 polymorph animals.cpp
+++ 10.3 polymorph animals.cpp
@@ -1,20 +1,47 @@
-// 10.3 polymorph animals.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*Make a class “Animal” with the abstract method speak()
+Make classes “dog” and “cat” that inherit from animal.
+Override the speak method
+Make the objects and call speak from main*/
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Animal {
+public:
+    virtual void speak() =0;
+};
+class Dog : public Animal {
+public:
+    void speak() override{
+        cout << "woof" << endl;
+    }
+};
+class Cat : public Animal {
+public:
+    void speak() override{
+        cout << "meow" << endl;
+    }
+};
+
+int main() {
+    while (1) {
+        int choice;
+        cout << "1. Dog" << endl << "2. Cat" << endl << "3. Exit" << endl;
+        cin >> choice;
+        if (choice == 1) {
+            Dog dog;
+            dog.speak();
+        }
+        else if (choice == 2) {
+            Cat cat;
+            cat.speak();
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice, try again!" << endl;
+        }
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
