a5c3ce0397dbb71bbbc51caba6d479d70235282b
martina <martina.bo@estudiantat.upc.edu>
2024-12-03 23:02:04 +0100

    pt 2 10.3 polymorph animals.cpp

--- 10.3 polymorph animals.cpp
+++ 10.3 polymorph animals.cpp
@@ -3,7 +3,7 @@ Make classes “dog” and “cat” that inherit from animal.
 Override the speak method
 Make the objects and call speak from main*/
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 class Animal {
@@ -37,6 +37,141 @@ int main() {
             cat.speak();
         }
         else if (choice == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice, try again!" << endl;
+        }
+    }
+    return 0;
+}*/
+
+/*Add the pure abstract method Eat() to animal class*/
+
+/*#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() =0;
+    void eat() {
+        cout<<"I am EAT!"<<endl;
+    }
+};
+class Dog : public Animal {
+public:
+    void speak() override{
+        cout << "woof" << endl;
+    }
+};
+class Cat : public Animal {
+public:
+    void speak() override{
+        cout << "meow" << endl;
+    }
+};
+
+int main() {
+    while (1) {
+        int choice0;
+        cout<<"1. SPEAK"<<endl << "2. EAT" << endl << "3. EXIST" << endl;
+        cin>>choice0;
+        if (choice0 == 1) {
+            int choice;
+            cout << "1. Dog" << endl << "2. Cat" << endl;
+            cin >> choice;
+            if (choice == 1) {
+                Dog dog;
+                dog.speak();
+            }
+            else if (choice == 2) {
+                Cat cat;
+                cat.speak();
+            }
+        }
+        else if (choice0 == 2) {
+            int choice;
+            cout << "1. Dog" << endl << "2. Cat" << endl << endl;
+            cin >> choice;
+            if (choice == 1) {
+                Dog dog;
+                dog.eat();
+            }
+            else if (choice == 2) {
+                Cat cat;
+                cat.eat();
+            }
+        }
+        else if (choice0 == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice, try again!" << endl;
+        }
+    }
+    return 0;
+}*/
+
+/*Add virtual destructor to Animal class*/
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() =0;
+    void eat() {
+        cout<<"I am EAT!"<<endl;
+    }
+    virtual void destructor() {
+        delete this;
+    };
+};
+class Dog : public Animal {
+public:
+    void speak() override{
+        cout << "woof" << endl;
+    }
+};
+class Cat : public Animal {
+public:
+    void speak() override{
+        cout << "meow" << endl;
+    }
+};
+
+int main() {
+    while (1) {
+        int choice0;
+        cout<<"1. SPEAK"<<endl << "2. EAT" << endl << "3. EXIST" << endl;
+        cin>>choice0;
+        if (choice0 == 1) {
+            int choice;
+            cout << "1. Dog" << endl << "2. Cat" << endl;
+            cin >> choice;
+            if (choice == 1) {
+                Dog dog;
+                dog.speak();
+            }
+            else if (choice == 2) {
+                Cat cat;
+                cat.speak();
+            }
+        }
+        else if (choice0 == 2) {
+            int choice;
+            cout << "1. Dog" << endl << "2. Cat" << endl << endl;
+            cin >> choice;
+            if (choice == 1) {
+                Dog dog;
+                dog.eat();
+            }
+            else if (choice == 2) {
+                Cat cat;
+                cat.eat();
+            }
+        }
+        else if (choice0 == 3) {
             break;
         }
         else {
